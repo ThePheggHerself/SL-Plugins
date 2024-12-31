9b704ec55bfbcfce025b0e5dbd9447e767920cb0
SpiderBuh <ijesbgujdbuheagfvues@gmail.com>
2024-12-31 23:13:11 +0200

    let the choas begin

---
+++ Choas/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+
+namespace Choas
+{
+    public class Plugin
+    {
+        [PluginEntryPoint("Choas", "1.0.0", "brings the choas to SL", "Dragon Inn Tech Team")]
+        public void OnPluginStart()
+        {
+            Log.Info($"Let the choas begin");
+        }
+    }
+}
