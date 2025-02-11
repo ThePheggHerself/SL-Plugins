2404ed55154bb2e9e63593397d05f01b02a788ab
Phoenix <thephoenixproject208@gmail.com>
2025-02-11 15:27:20 +0000

    :catderp: New beginings

--- CustomCommands/Features/SCPs/DamageAnnouncements/AnnouncementManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace CustomCommands.Features.SCPs.DamageAnnouncements
-{
-	public static class AnnouncementManager
-	{
-		public static Dictionary<string, float> ScpDamage = new Dictionary<string, float>();
-	}
-}
--- ExternalQuery/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExternalQuery
-{
-	public class Config
-	{
-		public bool Enabled { get; set; } = false;
-		public int Port { get; set; } = 8290;
-		public string Password { get; set; } = "NEVERGONNAGIVEYOUUP";
-	}
-}
